703df59e90ff108d7e4b9775ca3a4b1758ce5dbd
Anis <a_bertil@estin.dz>
2026-08-01 11:27:34 +0100

    modification

--- project/src/pages/Navbar.jsx
+++ project/src/pages/Navbar.jsx
@@ -39,7 +39,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           <div className="flex items-center gap-2">
-            <span className="text-lg sm:text-xl font-extrabold tracking-tight bg-blue-800 bg-clip-text text-transparent">
+            <span className="text-lg sm:text-xl font-extrabold tracking-tight bg-emerald-600 bg-clip-text text-transparent">
               {t("Algérie Télécom")}
             </span>
           </div>
